df065c8de4714533519d02f4ecf522678ca31cae
sidhartthhhhh <singhnishant294@gmail.com>
2025-07-10 20:19:42 +0530

    Pushed latest UI and responsive changes

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -124,8 +124,11 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
